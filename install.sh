9dfe846e053e81f6beef69a9e3dff5c50f739ae8
Marat <vanilla-thunder@users.noreply.github.com>
2018-07-15 14:51:12 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,9 +2,7 @@
 
 ln -f -s $(pwd)/a2reload  /bin/a2reload
 ln -f -s $(pwd)/a2restart /bin/a2restart
-ln -f -s $(pwd)/a2this    /bin/a2this
 ln -f -s $(pwd)/cleartmp  /bin/cleartmp
 ln -f -s $(pwd)/oxget     /bin/oxget
 ln -f -s $(pwd)/oxrights  /bin/oxrights
-ln -f -s $(pwd)/oxversion /bin/oxversion
 echo "symlinks created, do not delete this directory!"
